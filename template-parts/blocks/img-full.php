db5c2cd948b69aeb8fcdb4205d2ad9147d237962
Maksud Aliev <ma@nordsonne.de>
2022-12-06 17:58:02 +0100

    :hammer: img-full block: className + classes added from admin panel in one line, unused image var removed

--- template-parts/blocks/img-full.php
+++ template-parts/blocks/img-full.php
@@ -5,24 +5,22 @@
  * @param array $block The block settings and attributes.
  * @param string $content The block inner HTML (empty).
  * @param bool $is_preview True during AJAX preview.
- * @param   (int|string) $post_id The post ID this block is saved to.
+ * @param (int|string) $post_id The post ID this block is saved to.
  */
 
 // Create class attribute allowing for custom "className" values.
-$className = basename( __FILE__, '.php' );
-
-if ( ! empty( $block['className'] ) ) {
-	$className .= ' ' . $block['className'];
-}
-$images = get_field( 'slider' );
+$className = basename( __FILE__, '.php' ) . ( ! empty( $block['className'] ) ? ' ' . $block['className'] : '' );
+//frontend
 if ( ! is_admin() ) : ?>
 	<section class="section <?= $className ?>">
 		<div class="full-width">
 			<div class="section__inner img-full__img-wrap">
-				  <?= wp_get_attachment_image(get_field('img-full__img'), 'maxSize', '', ['class'=> 'img-full__img']) ?>
+				<?= wp_get_attachment_image( get_field( 'img-full__img' ), 'maxSize', '', [ 'class' => 'img-full__img' ] ) ?>
 			</div>
 		</div>
 	</section>
-<?php elseif ( is_admin() && ( $is_preview ?? '' ) ): ?>
+<?php
+//display preview html & on block hover
+elseif ( is_admin() && ( $is_preview ?? '' ) ): ?>
 	<img src="<?= get_template_directory_uri() . '/assets/imgs/previews/img.jpg' ?>" style="width: 100%; height: auto" alt="Preview of what the image full size custom block">
 <?php endif;
