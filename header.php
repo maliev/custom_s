5849188dfe10a28a767a25e82baafac42be9f319
Maksud Aliev <ma@nordsonne.de>
2022-12-07 15:05:16 +0100

    :sparkles: default favicon generated by realfavicongenerator

--- header.php
+++ header.php
@@ -16,6 +16,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= get_template_directory_uri() . '/assets/icons/apple-touch-icon.png' ?>">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= get_template_directory_uri() . '/assets/icons/favicon-32x32.png' ?>">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= get_template_directory_uri() . '/assets/icons/favicon-16x16.png' ?>">
+	<link rel="manifest" href="<?= get_template_directory_uri() . '/assets/icons/site.webmanifest' ?>">
+	<link rel="mask-icon" href="<?= get_template_directory_uri() . '/assets/icons/safari-pinned-tab.svg' ?>" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
