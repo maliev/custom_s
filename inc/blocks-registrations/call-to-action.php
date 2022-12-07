88e23cb883a434cfcfdd8002768fff45e44b9947
Maksud Aliev <ma@nordsonne.de>
2022-12-07 10:12:09 +0100

    :sparkles: new custom block "cta" or "call-to-action" with simple html template (without any styles)

---
+++ inc/blocks-registrations/call-to-action.php
@@ -0,0 +1,22 @@
+<?php
+// Register a call-to-action block.
+acf_register_block_type( [
+	'name'            => 'call-to-action',
+	'title'           => __( 'Call-to-Action' ),
+	'description'     => __( 'A custom block: call-to-action.' ),
+	'render_template' => 'template-parts/blocks/call-to-action.php',
+	'mode'            => 'edit',
+	'align'           => 'full',
+	'category'        => 'custom-blocks',
+	'icon'            => 'button',
+	'keywords'        => [ 'Call-to-Action', 'call-to-action', 'Newsletter', 'Contact', 'Kontakt' ],
+	'example'         => [
+		'attributes' => [
+			'mode' => 'preview',
+			'data' => [
+				'_is_preview' => 'true',
+			],
+		],
+	],
+] );
+
---
+++ template-parts/blocks/call-to-action.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Call-to-Action Block Template.
+ *
+ * @param array $block The block settings and attributes.
+ * @param string $content The block inner HTML (empty).
+ * @param bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// create class attribute allowing for custom "className" values && adding classes from admin panel if exist
+$className     = basename( __FILE__, '.php' ) . ( ! empty( $block['className'] ) ? ' ' . $block['className'] : '' );
+$bg           = get_field( 'call-to-action__bg' );
+$title        = get_field( 'call-to-action__title' );
+$subline      = get_field( 'call-to-action__subline' );
+$button       = get_field( 'call-to-action__button' );
+//frontend
+if ( ! is_admin() ) : ?>
+	<section class="section has--background cta <?= $className ?>">
+		<div class="full-width bg--<?= $bg ?>">
+			<div class="container cta__inner">
+				<div class="row g-0">
+					<div class="col-md-7">
+						<div class="cta__left">
+							<div class="h2 cta__title"><?= $title ?></div>
+							<div class="h3 cta__subline"><?= $subline ?></div>
+						</div>
+					</div>
+					<div class="col-md-5">
+						<div class="cta__right">
+							<?php if ( $button ): ?>
+								<a class="button cta__button" href="<?= $button['url'] ?>" title="<?= $button['title'] ?>" target="<?= $button['target'] ?>"><?= $button['title'] ?></a>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<?php
+//display preview html & on block hover
+elseif ( is_admin() && ( $is_preview ?? '' ) ): ?>
+	<img src="<?= get_template_directory_uri() . '/assets/imgs/previews/call-to-action.jpg' ?>" style="width: 100%; height: auto" alt="Preview of what the call-to-action (cta) custom block">
+<?php endif;
