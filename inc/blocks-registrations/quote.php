f7d9372e9826f0d8d2dc7f93ac942679c1103d2e
Maksud Aliev <ma@nordsonne.de>
2023-01-02 15:27:51 +0100

    :sparkles: new quote block added + basic alignment classes

---
+++ inc/blocks-registrations/quote.php
@@ -0,0 +1,28 @@
+<?php
+//register quote acf block
+acf_register_block_type( [
+	'name'            => 'quote',
+	'title'           => __( 'Quote' ),
+	'discription'     => __( 'A custom block: quote' ),
+	'render_template' => 'template-parts/blocks/quote.php',
+	'mode'            => 'edit',
+	'category'        => 'content',
+	'icon'            => 'format-quote',
+	'keywords'        => [ 'Zitat', 'quote' ],
+	'example'         => [
+		'attributes' => [
+			'mode' => 'preview',
+			'data' => [
+				'_is_preview' => 'true',
+			],
+		],
+	],
+	'supports'        => [
+		'align' => [ 'left', 'right', 'center', 'wide', 'full' ],
+		'color' => [
+			'background' => false,
+			'gradients'  => false,
+			'text'       => true,
+		],
+	],
+] );
---
+++ template-parts/blocks/quote.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Quote Block Template
+ *
+ * @param array $block The block settings and attributes.
+ * @param string $content The block inner HTML (empty).
+ * @param bool $is_preview True during AJAX preview.
+ * @param (int|string) $post_id The post ID this block is saved to.
+ */
+//create class attribute for main section tag with file name as class && add classes from admin panel if exist
+$className = basename( __FILE__, '.php' ) . ( ! empty( $block['className'] ) ? ' ' . $block['className'] : '' );
+//add block alignment as class
+$alignment = ( !empty( $block['align'] ) ? ( ' is--' . $block['align'] ) : '' );
+//front
+if ( ! is_admin() ) : ?>
+	<section class="section <?= $className ?>">
+		<div class="row<?= $alignment ?>">
+			<figure class="col-md-6 quote__inner">
+				<blockquote><?= get_field( 'quote__text' ); ?></blockquote>
+				<?php if ( $src = get_field( 'quote__source' ) ): ?>
+					<figcaption><?= $src ?></figcaption>
+				<?php endif; ?>
+				<?php if ( $btn = get_field( 'quote__button' ) ): ?>
+					<a class="button quote__button" href="<?= $btn['url'] ?>"<?= $btn['target'] === '_blank' ? ' target="_blank"' : '' ?>><?= $btn['title'] ?></a>
+				<?php endif; ?>
+			</figure>
+		</div>
+	</section>
+<?php elseif ( is_admin() && ( $is_preview ?? '' ) ): ?>
+	<img src="<?= get_template_directory_uri() . '/assets/imgs/previews/quote.jpg' ?>" alt="Preview of the quote block">
+<?php endif;
